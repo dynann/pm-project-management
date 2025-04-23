3f6e3e0028f2fd44603049f6983cc2b74e963ab2
rthjr <sovannarithphan9@gmail.com>
2025-04-23 11:43:47 +0700

    fix CORS

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,22 @@
 <?php
+
+
 return [
-    'paths' => ['api/*'],
+
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ['*'],
+
+    'allowed_origins' => ['http://localhost:3000'], // <-- Your frontend
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => true,
-]
-    ?>
+
+];
