4c2c485edfd0a67506defb01971fbdccefe42370
rthjr <sovannarithphan9@gmail.com>
2025-05-22 15:27:29 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -3,14 +3,13 @@
 
 return [
 
-    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'broadcasting/auth', '*'],
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
         'http://localhost:3000',
         'https://pm-project-management-frontend-dbuw.vercel.app',
-        '*'
     ],
 
     'allowed_origins_patterns' => [],
